30843bf73a3c87bc3f93471d2a230ac2951d8bd2
rElaX299 <2399984023@qq.com>
2025-08-14 16:56:41 +0800

    a better scrabble solution

--- Lesson02/scrabble/scrabble.c
+++ Lesson02/scrabble/scrabble.c
@@ -1,8 +1,11 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
+const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int get_score(char *str);
 int parse_char(char c);
@@ -19,18 +22,14 @@ int main()
     else if (player1_score < player2_score)
         printf("Player 2 wins!\n");
     else
-        printf("Tie!");
+        printf("Tie!\n");
 }
 
 int get_score(char *str)
 {
     int score = 0;
     for (int i = 0; i < strlen(str); i++) {
-        char c = str[i];
-        if (c >= 'A' && c <= 'Z') {
-            c = c - ('A' - 'a');
-        }
-        score += parse_char(c);
+        score += parse_char(str[i]);
     }
 
     return score;
@@ -39,43 +38,9 @@ int get_score(char *str)
 int parse_char(char c)
 {
     int result = 0;
-    switch (c)
-    {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'l':
-        case 'n':
-        case 'o':
-        case 'r':
-        case 's':
-        case 't':
-        case 'u':
-            return 1;
-        case 'd':
-        case 'g':
-            return 2;
-        case 'b':
-        case 'c':
-        case 'm':
-        case 'p':
-            return 3;
-        case 'f':
-        case 'h':
-        case 'v':
-        case 'w':
-        case 'y':
-            return 4;
-        case 'k':
-            return 5;
-        case 'j':
-        case 'x':
-            return 8;
-        case 'q':
-        case 'z':
-            return 10;
-        default:
-            break;
-    }
-    return 0;
+    if (isupper(c))
+        result = POINTS[c - 'A'];
+    else if (islower(c))
+        result = POINTS[c - 'a'];
+    return result;
 }

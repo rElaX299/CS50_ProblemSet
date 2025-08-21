d90919f18d06979213171775f503e68f90893f2d
rElaX299 <2399984023@qq.com>
2025-08-21 14:30:19 +0800

    sql movie 1-10

---
+++ Lesson07/movies/6.sql
@@ -0,0 +1,6 @@
+-- SELECT avg(rating) FROM ratings WHERE movie_id IN (
+-- SELECT id FROM movies WHERE year = 2012);
+
+SELECT avg(rating) FROM ratings
+JOIN movies ON movies.id = ratings.movie_id
+WHERE movies.year = 2012;

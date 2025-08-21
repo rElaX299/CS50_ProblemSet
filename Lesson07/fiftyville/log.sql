6da40381436eaf6976753c9d5feb3e539e54f433
rElaX299 <2399984023@qq.com>
2025-08-21 18:09:01 +0800

    add fiftyville solution

--- Lesson07/fiftyville/log.sql
+++ Lesson07/fiftyville/log.sql
@@ -13,7 +13,173 @@ SELECT * FROM interviews
 WHERE year = 2024 AND month = 7 AND day = 28
 AND transcript LIKE '%bakery%';
 
--- Find 
+-- Find suspicious license plate
 SELECT * FROM bakery_security_logs
 WHERE year = 2024 AND month = 7 AND day = 28
-AND hour = 10 AND (minute > 4 AND minute < 26);
+AND hour = 10 AND (minute > 4 AND minute < 26)
+AND activity = 'exit';
+
+-- Find suspect based on license
+SELECT * FROM people WHERE license_plate IN
+(
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2024 AND month = 7 AND day = 28
+    AND hour = 10 AND (minute > 4 AND minute < 26)
+    AND activity = 'exit'
+);
+
+-- Find suspicious account number
+SELECT * FROM atm_transactions
+WHERE year = 2024 AND month = 7 AND day = 28
+AND atm_location LIKE '%Leggett%'
+AND transaction_type = 'withdraw';
+
+-- Find suspect based on account number
+SELECT person_id FROM bank_accounts WHERE account_number IN
+(
+    SELECT account_number FROM atm_transactions
+    WHERE year = 2024 AND month = 7 AND day = 28
+    AND atm_location LIKE '%Leggett%'
+    AND transaction_type = 'withdraw'
+);
+
+-- Find suspect based on account
+SELECT * FROM people WHERE id IN
+(
+    SELECT person_id FROM bank_accounts WHERE account_number IN
+    (
+        SELECT account_number FROM atm_transactions
+        WHERE year = 2024 AND month = 7 AND day = 28
+        AND atm_location LIKE '%Leggett%'
+        AND transaction_type = 'withdraw'
+    )
+);
+
+-- Find Fiftyville airport id
+SELECT * FROM airports WHERE city = 'Fiftyville';
+
+-- Find earliest plane
+SELECT * FROM flights 
+WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+AND year = 2024 AND month = 7 AND day = 29
+ORDER BY hour, minute
+LIMIT 1;
+
+-- Find escape destination
+SELECT city FROM airports WHERE id IN
+(
+    SELECT destination_airport_id FROM flights 
+    WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+    AND year = 2024 AND month = 7 AND day = 29
+    ORDER BY hour, minute
+    LIMIT 1
+);
+
+-- FIND passengers information
+SELECT passport_number FROM passengers WHERE flight_id = 
+(
+    SELECT id FROM flights 
+    WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+    AND year = 2024 AND month = 7 AND day = 29
+    ORDER BY hour, minute
+    LIMIT 1
+);
+
+-- FIND suspect based on flight
+SELECT * FROM people WHERE passport_number IN
+(
+    SELECT passport_number FROM passengers WHERE flight_id = 
+    (
+        SELECT id FROM flights 
+        WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+        AND year = 2024 AND month = 7 AND day = 29
+        ORDER BY hour, minute
+        LIMIT 1
+    )
+);
+
+-- Find suspicious phone number based on phone call
+SELECT caller FROM phone_calls WHERE year = 2024 AND month = 7 AND day = 28 AND duration < 60;
+
+-- Find suspect based on phone number
+SELECT * FROM people WHERE phone_number IN
+(
+    SELECT caller FROM phone_calls WHERE year = 2024 AND month = 7 AND day = 28 AND duration < 60
+);
+
+-- Find suspect based on all
+SELECT name FROM people 
+WHERE phone_number IN
+(
+    SELECT caller FROM phone_calls WHERE year = 2024 AND month = 7 AND day = 28 AND duration < 60
+)
+AND passport_number IN
+(
+    SELECT passport_number FROM passengers WHERE flight_id = 
+    (
+        SELECT id FROM flights 
+        WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+        AND year = 2024 AND month = 7 AND day = 29
+        ORDER BY hour, minute
+        LIMIT 1
+    )
+)
+AND id IN
+(
+    SELECT person_id FROM bank_accounts WHERE account_number IN
+    (
+        SELECT account_number FROM atm_transactions
+        WHERE year = 2024 AND month = 7 AND day = 28
+        AND atm_location LIKE '%Leggett%'
+        AND transaction_type = 'withdraw'
+    )
+)
+AND license_plate IN
+(
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2024 AND month = 7 AND day = 28
+    AND hour = 10 AND (minute > 4 AND minute < 26)
+    AND activity = 'exit'
+);
+
+-- Find ACCOMPLICE
+SELECT name FROM people WHERE phone_number IN
+(
+    SELECT receiver FROM phone_calls WHERE year = 2024 AND month = 7 AND day = 28 AND duration < 60
+    AND caller IN 
+    (
+        SELECT phone_number FROM people 
+        WHERE phone_number IN
+        (
+            SELECT caller FROM phone_calls WHERE year = 2024 AND month = 7 AND day = 28 AND duration < 60
+        )
+        AND passport_number IN
+        (
+            SELECT passport_number FROM passengers WHERE flight_id = 
+            (
+                SELECT id FROM flights 
+                WHERE origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville')
+                AND year = 2024 AND month = 7 AND day = 29
+                ORDER BY hour, minute
+                LIMIT 1
+            )
+        )
+        AND id IN
+        (
+            SELECT person_id FROM bank_accounts WHERE account_number IN
+            (
+                SELECT account_number FROM atm_transactions
+                WHERE year = 2024 AND month = 7 AND day = 28
+                AND atm_location LIKE '%Leggett%'
+                AND transaction_type = 'withdraw'
+            )
+        )
+        AND license_plate IN
+        (
+            SELECT license_plate FROM bakery_security_logs
+            WHERE year = 2024 AND month = 7 AND day = 28
+            AND hour = 10 AND (minute > 4 AND minute < 26)
+            AND activity = 'exit'
+        )
+    )
+);
